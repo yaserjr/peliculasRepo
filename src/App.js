0d4817c5d274bffff972dca348168cf4ec544d73
yaserjr <yaserjimenez@gmail.com>
2022-01-04 16:10:07 -0500

    subida 01 04012022

--- src/App.js
+++ src/App.js
@@ -10,7 +10,41 @@ import { useState } from 'react';
 
 function App() {
   const [paginaActual, setPaginaActual] = useState(1);
+  const Total_Paginas = 2;
+
   let pelicula = peliculasJson;
+
+  const buscarPelicula = async() => {
+    let url = 'https://cors-anywhere.herokuapp.com/https://lucasmoy.dev/data/react/peliculas.json';
+    
+    let respuesta = await fetch(url,{
+      "method": 'GET',
+    //  "mode": 'no-cors',
+      "headers": {
+        "Accept": 'application/json',
+        "Content-Type": 'application/json'     ,   
+        "Origin": 'https://lucasmoy.dev/'
+      } 
+    });
+   
+    let json = await respuesta.json(); 
+    debugger;
+  }
+  buscarPelicula();
+
+  //con esta funcion determinamos el numero de peliculas a mostrar por pagina
+  const cargarPeliculas = () =>{
+    pelicula = pelicula.slice((paginaActual-1)*Total_Paginas,paginaActual*Total_Paginas);
+  }
+
+  //con esta funcion determinamos el numero total de paginas
+  const getTotalDePaginas = () => {
+    let cantidadTotalPaginas = peliculasJson.length;
+    return Math.ceil(cantidadTotalPaginas/Total_Paginas);//Math.ceil redondea para arriba ejem: 2.1 = 3;Math.round redondea para abajo ejem: 2.1 = 2
+  }
+
+  cargarPeliculas();
+
   return (
     <PageWrapper>
 
@@ -21,7 +55,7 @@ function App() {
     </Pelicula>   
    ) 
    }
-   <Paginacion pagina={paginaActual} total={4} onChange={(pagina) => {setPaginaActual(pagina)}}/>
+   <Paginacion pagina={paginaActual} total={getTotalDePaginas()} onChange={(pagina) => {setPaginaActual(pagina)}}/>
     </PageWrapper>
     
       
